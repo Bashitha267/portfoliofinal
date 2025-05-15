1ab4303fa1f53e89327bd1bdb5ee999ab9112f76
Nimesh Bashitha <2022csc023@univ.jfn.ac.lk>
2025-05-15 18:34:37 +0530

    profile pic updated

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -130,7 +130,7 @@ const Home: React.FC<HomeProps> = ({ backgroundY, opacity, parallaxY }) => {
               }}
             />
             <motion.img
-              src="https://res.cloudinary.com/dnfbik3if/image/upload/v1741190246/IMG_5269_zilu2h.jpg"
+              src="https://res.cloudinary.com/dnfbik3if/image/upload/v1747314247/2022CSC023Bashitha.jpeg_bw5woe.jpg"
               alt="Hero Image"
               className="w-full h-full object-cover rounded-2xl shadow-2xl relative z-10"
               style={{
